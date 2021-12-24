fe63cdd897f3b740d113d14eb802e2b6b11b91db
Alexius <alexius666@yandex.ru>
2021-12-24 18:30:21 +0500

    Done

--- Lesson1-3.java
+++ Lesson1-3.java
@@ -1,12 +1,37 @@
+import java.util.Scanner;
+
 class Main {
     public static void main(String[] args) {
-        int width = 16; // ширина прямоугольника
-        int height = 21; // высота прямоугольника
-        int square = width * height; // вычисление площади прямоугольника
-        int perimeter = 2 * width + 2 * height; // // вычисление перимерта прямоугольника
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        boolean ans=false;
 
-        System.out.println("Площадь прямоугольника равна " + square);
-        System.out.println("Периметр прямоугольника равен " + perimeter);
+        System.out.printf("Число %d: ", a);
+        if (a%2==0) System.out.print("чётное,"); else System.out.print("нечётное,");
+        if (a==1) System.out.print(" единица(имеет один натуральный делитель)");
+        else
+            if ((a==2) || (a==3)) ans=true;
+        else
+            if (a<=1 || a%2==0 || a%3==0) ans=false;
+            else {
+                for (int i = 5; i * i <= a; i += 6) {
+                    if (a % i == 0 || a % (i + 2) == 0) ans=false;
+                }
+                ans=true;
+            }
+
+        if (ans && a!=1) System.out.print(" простое.");
+        if (!ans && a!=1) System.out.print(" составное.");
     }
-  
-}  
+
+}
+
+//String a = args[0];
+//String b = args[1];
+//int oper1=Integer.valueOf(a);
+//int oper2=Integer.valueOf(b);
+//int sum=oper1+oper2;
+//System.out.println("Введенное число " + a);
+// System.out.println("Чётное ");
+//int b = scanner.nextInt();
+//System.out.printf("Введенное число %d и %d", b, a);
