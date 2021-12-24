ce0078c491fa925997ee1d64c1a8c363746ede97
alexius666 <65847704+alexius666@users.noreply.github.com>
2021-12-24 15:26:17 +0500

    Create Lesson1-3.java

---
+++ Lesson1-3.java
@@ -0,0 +1,12 @@
+class Main {
+    public static void main(String[] args) {
+        int width = 16; // ширина прямоугольника
+        int height = 21; // высота прямоугольника
+        int square = width * height; // вычисление площади прямоугольника
+        int perimeter = 2 * width + 2 * height; // // вычисление перимерта прямоугольника
+
+        System.out.println("Площадь прямоугольника равна " + square);
+        System.out.println("Периметр прямоугольника равен " + perimeter);
+    }
+  
+}  
